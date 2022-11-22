3bb269cb595b85c699f45265243fb83a1143a39b
jogp10 <jogp10@gmail.com>
2022-11-22 20:03:01 +0000

    comments

--- src/board.pl
+++ src/board.pl
@@ -1,10 +1,11 @@
 % Initiate the board
-
 /* e.g.
+  -2 - out of bounds
    0 - empty
    1 - player 1
    2 - player 2 
 */
+
 initBoard(Board) :-
     Board = 
        [[0 , 0, 0, 0, 0,-2,-2 ,-2,-2],
@@ -18,8 +19,22 @@ initBoard(Board) :-
         [-2,-2,-2,-2, 0, 0, 0 ,0 , 0]
 ].
     
+
 % Display the board
-%  [[0 , 0,0 ,0 ,0 ,-2,-2 ,-2,-2],[0 , 0,0 ,0 ,0 ,0 ,-2 ,-2,-2],[0 , 0,0 ,0 ,0 ,0 , 0 ,-2,-2],[0 , 0,0 ,0 ,0 ,0 , 0 ,0 ,-2],[0 , 0,0 ,0 ,0 ,0 , 0 ,0 , 0],[-2, 0,0 ,0 ,0 ,0 , 0 ,0 , 0],[-2,-2,0 ,0 ,0 ,0 , 0 ,0 , 0],[-2,-2,-2,0 ,0 ,0 , 0 ,0 , 0],[-2,-2,-2,-2,0 ,0 , 0 ,0 , 0]].
+/* e.g.
+    -     1 2 3 4 5
+    I     - - - - -
+    H    - - - - - - 6
+    G   - - - - - - - 7
+    F  - - - - - - - - 8
+    E - - - - - - - - - 9
+    D  - - - - - - - - 8
+    C   - - - - - - - 7
+    B    - - - - - - 6
+    A     - - - - -
+          1 2 3 4 5
+
+*/
 
 displayBoard(Board) :-
     write('  0 1 2 3 4 5 6 7 8 9'), nl,
@@ -34,6 +49,12 @@ displayBoard([H|T], N) :-
     NewN is N + 1,
     displayBoard(T, NewN).
 
+
+% Display a row of the board
+/* e.g.
+    A     - - - - -
+*/
+
 displayRow([]) :-
     nl.
 displayRow([H|T]) :-
@@ -43,7 +64,10 @@ displayRow([H|T]) :-
     (H == 1 -> write('1 '));
     (H == 2 -> write('2 '))),
     displayRow(T).
-    
+ 
+
+% Add spacing to the board
+
 spacing(0) :- write('    ').
 spacing(1) :- write('   ').
 spacing(2) :- write('  ').
