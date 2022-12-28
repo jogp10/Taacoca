dfa7faca4c8bfdf3b4f5c59c69e50348966cfd7b
jogp10 <jogp10@gmail.com>
2022-12-28 23:24:22 +0000

    computer easy mode done

--- src/computer.pl
+++ src/computer.pl
@@ -1,12 +1,18 @@
-:- use_module(library(random)).
 :- ensure_loaded(board).
 
 % Select randomly three pieces from the board
-computer_easy_move(Board, Player, (X1, Y1), (X2, Y2), (X3, Y3)) :- 
+computer_easy_move(Board, Player, [(X1, Y1), (X2, Y2), (X3, Y3)], Direction) :-
     get_player_pieces(Board, Player, Pieces),
-    pick_piece(3, Pieces, [(X1, Y1), (X2, Y2), (X3, Y3)]),
-    valid_moves(Board, [(X1, Y1), (X2, Y2), (X3, Y3)], ValidMoves).
+    length(Pieces, Count),
+    (Count > 3 -> PiecesToPick = 3 ; PiecesToPick = Count),
+    pick_piece(PiecesToPick, Pieces, [(X4, Y4), (X5, Y5), (X6, Y6)]),
+    write('Computer selected pieces: '), write([(Y4, X4), (Y5, X5), (Y6, X6)]), nl,
+    valid_moves(Board, [(X4, Y4), (X5, Y5), (X6, Y6)], ValidMoves),
+    length(ValidMoves, Length),
+    (Length > 0 -> (X1 = X4, Y1 = Y4, X2 = X5, Y2 = Y5, X3 = X6, Y3 = Y6, random_member(Direction, ValidMoves), write('Computer selected direction: '), write(Direction), nl)
+    ; computer_easy_move(Board, Player, [(X1, Y1), (X2, Y2), (X3, Y3)], Direction)).
 
+pick_piece(0, _, _).
 pick_piece(0, _, []).
 pick_piece(Count, From, [X| Rest]) :-
     Count > 0,
--- src/main.pl
+++ src/main.pl
@@ -1,5 +1,7 @@
 :- ensure_loaded(board).
+:- ensure_loaded(computer).
 :- use_module(library(lists)).
+:- use_module(library(random)).
 
 % Display the main menu
 
@@ -17,20 +19,20 @@ play :-
     write('1. Easy'), nl,
     write('2. Hard'), nl,
     read(Difficulty),
-    choose_difficulty(Difficulty, c),
-    play(p, c));
+    choose_difficulty(Difficulty, C),
+    play(p, C));
     (Option == 3 -> 
     write('Choose the difficulty for Computer 1:'), nl,
     write('1. Easy'), nl,
     write('2. Hard'), nl,
     read(Difficulty1),
-    choose_difficulty(Difficulty1, c1),
+    choose_difficulty(Difficulty1, C1),
     write('Choose the difficulty for Computer 2:'), nl,
     write('1. Easy'), nl,
     write('2. Hard'), nl,
     read(Difficulty2),
-    choose_difficulty(Difficulty2, c2),
-    play(c1, c2));
+    choose_difficulty(Difficulty2, C2),
+    play(C1, C2));
     (Option == 4 -> write('Bye!'), nl)).
 
 % Choose the computer difficulty
@@ -50,19 +52,19 @@ play(Player1, Player2) :-
 
 play(Player1, Player2, Board, Turn) :-
 
-    %(Player1 == c -> write('Computer 1 turn:'), nl, computerMove(Board, X1, Y1, X2, Y2)),
+    (Player1 == c1 -> write('Computer 1 turn:'), nl, computer_easy_move(Board, 1, [(X1, Y1), (X2, Y2), (X3, Y3)], Direction);
     (Player1 == p -> write('Player 1 turn (Pieces: X):'), nl, pieces_to_move(Board, 1, (X1, Y1), (X2, Y2), (X3, Y3)),
     valid_moves(Board, [(X1, Y1), (X2, Y2), (X3, Y3)], ValidMoves),
-    move_dir(Direction, ValidMoves),
+    move_dir(Direction, ValidMoves))),
     count_pieces(Board, 1, Count),
 
-    neighbor(X1, Y1, Direction, (A, B)),
-    neighbor(X2, Y2, Direction, (C, D)),
-    neighbor(X3, Y3, Direction, (E, F)),
+    (Count >= 1, neighbor(X1, Y1, Direction, (A, B))),
+    (Count >= 2, neighbor(X2, Y2, Direction, (C, D))),
+    (Count >= 3, neighbor(X3, Y3, Direction, (E, F))),
 
     (Count >= 1 -> move_piece(Board, (Y1, X1), (B, A), NewBoard);true),
     (Count >= 2 -> move_piece(NewBoard, (Y2, X2), (D, C), NewBoard2);true),
-    (Count >= 3 -> move_piece(NewBoard2, (Y3, X3), (F, E), NewBoard4);true)),
+    (Count >= 3 -> move_piece(NewBoard2, (Y3, X3), (F, E), NewBoard4);true),
 
     ((Count == 1, NewBoard3 = NewBoard);
     (Count == 2, NewBoard3 = NewBoard2);
@@ -74,25 +76,23 @@ play(Player1, Player2, Board, Turn) :-
     ((count_pieces(NewBoard3, 2, Count_Win), Count_Win == 0 -> write('Player 1 wins!'), nl, halt);
     (check_player_in_win_row(1, NewBoard3) -> write('Player 1 wins!'), nl, halt);true),
 
-
-    % (Player2 == c -> write('Computer 2 turn:'), nl, computerMove(NewBoard, X1, Y1, X2, Y2)),
+    (Player2 == c1 -> write('Computer 2 turn:'), nl, computer_easy_move(Board, 2, [(X4, Y4), (X5, Y5), (X6, Y6)], Direction2);
     (Player2 == p -> write('Player 2 turn (Pieces: O):'), nl, pieces_to_move(Board, 2, (X4, Y4), (X5, Y5), (X6, Y6)),
     valid_moves(NewBoard3, [(X4, Y4), (X5, Y5), (X6, Y6)], ValidMoves2),
-    move_dir(Direction2, ValidMoves2),
+    move_dir(Direction2, ValidMoves2))),
     count_pieces(NewBoard3, 2, Count2),
+    write('Count2: '), write(Count2), nl,
+    (Count2 >= 1, neighbor(X4, Y4, Direction2, (A2, B2))),
+    (Count2 >= 2, neighbor(X5, Y5, Direction2, (C2, D2))),
+    (Count2 >= 3, neighbor(X6, Y6, Direction2, (E2, F2))),
 
-    neighbor(X4, Y4, Direction2, (A2, B2)),
-    (Count2 >= 1, move_piece(NewBoard3, (Y4, X4), (B2, A2), NewBoard4)),
+    (Count2 >= 1, move_piece(NewBoard3, (Y4, X4), (B2, A2), NewBoard5)),
+    (Count2 >= 2, move_piece(NewBoard5, (Y5, X5), (D2, C2), NewBoard7)),
+    (Count2 >= 3, move_piece(NewBoard7, (Y6, X6), (F2, E2), NewBoard8)),
 
-    neighbor(X5, Y5, Direction2, (C2, D2)),
-    (Count2 >= 2, move_piece(NewBoard4, (Y5, X5), (D2, C2), NewBoard5)),
-
-    neighbor(X6, Y6, Direction2, (E2, F2)),
-    (Count2 >= 3, move_piece(NewBoard5, (Y6, X6), (F2, E2), NewBoard7))),
-
-    ((Count == 1, NewBoard6 = NewBoard4);
-    (Count == 2, NewBoard6 = NewBoard5);
-    (Count >= 3, NewBoard6 = NewBoard7)),
+    ((Count == 1, NewBoard6 = NewBoard5);
+    (Count == 2, NewBoard6 = NewBoard7);
+    (Count >= 3, NewBoard6 = NewBoard8)),
 
     display_game(NewBoard6),
 
