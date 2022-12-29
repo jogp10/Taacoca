05e565affa24f70972e73cea6eeb29881e8ee205
RicardoCavalheiro123 <up202005103@fe.up.pt>
2022-12-29 15:27:13 +0000

    moving bug fix

--- src/main.pl
+++ src/main.pl
@@ -56,21 +56,33 @@ play(Player1, Player2, Board, Turn) :-
     (Player1 == c1 -> write('Computer 1 turn:'), nl, computer_easy_move(Board, 1, [(X1, Y1), (X2, Y2), (X3, Y3)], Direction);
     (Player1 == p -> write('Player 1 turn (Pieces: X):'), nl, pieces_to_move(Board, 1, (X1, Y1), (X2, Y2), (X3, Y3)),
     valid_moves(Board, [(X1, Y1), (X2, Y2), (X3, Y3)], ValidMoves),
+
     move_dir(Direction, ValidMoves))),
+
     count_pieces(Board, 1, Count),
+    write('Count: '), write(Count), nl,
+    (Count >= 1 -> write('1') , nl, neighbor(X1, Y1, Direction, (A, B));true),
+    (Count >= 2 -> write('2') , nl, neighbor(X2, Y2, Direction, (C, D));true),
+    (Count >= 3 -> write('3') , nl, neighbor(X3, Y3, Direction, (E, F));true),
+    
+    % Remove pieces
+    (Count >= 1 -> remove_piece(Board, (Y1, X1), (B, A), Piece, NewBoard);true),
+    (Count >= 2 -> remove_piece(NewBoard, (Y2, X2), (D, C), Piece, NewBoard2);true),
+    (Count >= 3 -> remove_piece(NewBoard2, (Y3, X3), (F, E), Piece, NewBoard4);true),
 
-    (Count >= 1, neighbor(X1, Y1, Direction, (A, B))),
-    (Count >= 2, neighbor(X2, Y2, Direction, (C, D))),
-    (Count >= 3, neighbor(X3, Y3, Direction, (E, F))),
 
-    (Count >= 1 -> move_piece(Board, (Y1, X1), (B, A), NewBoard);true),
-    (Count >= 2 -> move_piece(NewBoard, (Y2, X2), (D, C), NewBoard2);true),
-    (Count >= 3 -> move_piece(NewBoard2, (Y3, X3), (F, E), NewBoard4);true),
+    ((Count == 1, New = NewBoard);
+    (Count == 2, New = NewBoard2);
+    (Count >= 3, New = NewBoard4)),
 
-    ((Count == 1, NewBoard3 = NewBoard);
-    (Count == 2, NewBoard3 = NewBoard2);
-    (Count >= 3, NewBoard3 = NewBoard4)),
+    % Move pieces
+    (Count >= 1 -> move_piece(New, (Y1, X1), (B, A), Piece, NewB1);true),
+    (Count >= 2 -> move_piece(NewB1, (Y2, X2), (D, C), Piece, NewB2);true),
+    (Count >= 3 -> move_piece(NewB2, (Y3, X3), (F, E), Piece, NewB3);true),
 
+    ((Count == 1, NewBoard3 = NewB1);
+    (Count == 2, NewBoard3 = NewB2);
+    (Count >= 3, NewBoard3 = NewB3)),
     display_game(NewBoard3),
 
     %check if the game is over
@@ -83,17 +95,30 @@ play(Player1, Player2, Board, Turn) :-
     move_dir(Direction2, ValidMoves2))),
     count_pieces(NewBoard3, 2, Count2),
     write('Count2: '), write(Count2), nl,
-    (Count2 >= 1, neighbor(X4, Y4, Direction2, (A2, B2))),
-    (Count2 >= 2, neighbor(X5, Y5, Direction2, (C2, D2))),
-    (Count2 >= 3, neighbor(X6, Y6, Direction2, (E2, F2))),
 
-    (Count2 >= 1, move_piece(NewBoard3, (Y4, X4), (B2, A2), NewBoard5)),
-    (Count2 >= 2, move_piece(NewBoard5, (Y5, X5), (D2, C2), NewBoard7)),
-    (Count2 >= 3, move_piece(NewBoard7, (Y6, X6), (F2, E2), NewBoard8)),
+    (Count2 >= 1 -> neighbor(X4, Y4, Direction2, (A2, B2));true),
+    (Count2 >= 2 -> neighbor(X5, Y5, Direction2, (C2, D2));true),
+    (Count2 >= 3 -> neighbor(X6, Y6, Direction2, (E2, F2)); true),
 
-    ((Count == 1, NewBoard6 = NewBoard5);
-    (Count == 2, NewBoard6 = NewBoard7);
-    (Count >= 3, NewBoard6 = NewBoard8)),
+
+
+    % Remove pieces
+    (Count2 >= 1, remove_piece(NewBoard3, (Y4, X4), (B2, A2), Piece2, NewBoard5)),
+    (Count2 >= 2, remove_piece(NewBoard5, (Y5, X5), (D2, C2), Piece2, NewBoard7)),
+    (Count2 >= 3, remove_piece(NewBoard7, (Y6, X6), (F2, E2), Piece2, NewBoard8)),
+
+    ((Count2 == 1, New2 = NewBoard5);
+    (Count2 == 2, New2 = NewBoard7);
+    (Count2 >= 3, New2 = NewBoard8)),
+   
+    % Move pieces
+    (Count2 >= 1, move_piece(New2, (Y4, X4), (B2, A2), Piece2, N1)),
+    (Count2 >= 2, move_piece(N1, (Y5, X5), (D2, C2), Piece2, N2)),
+    (Count2 >= 3, move_piece(N2, (Y6, X6), (F2, E2), Piece2, N3)),
+
+    ((Count2 == 1, NewBoard6 = N1);
+    (Count2 == 2, NewBoard6 = N2);
+    (Count2 >= 3, NewBoard6 = N3)),
 
     display_game(NewBoard6),
 
