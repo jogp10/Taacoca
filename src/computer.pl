9c29b3a7a90f3a8dbaefb902c4dab68fdcc502e5
jogp10 <jogp10@gmail.com>
2022-12-29 15:46:34 +0000

    Ready to deploy

--- src/computer.pl
+++ src/computer.pl
@@ -10,7 +10,14 @@ computer_easy_move(Board, Player, [(X1, Y1), (X2, Y2), (X3, Y3)], Direction) :-
     (Count == 1 -> X5 = X4, Y5 = Y4, X6 = X4, Y6 = Y4;
     Count == 2 -> X6 = X4, Y6 = Y4; true),
 
-    write('Computer selected pieces: '), write([(Y4, X4), (Y5, X5), (Y6, X6)]), nl,
+    offset(Y4, Offset1),
+    offset(Y5, Offset2),
+    offset(Y6, Offset3),
+    X10 is X4 - Offset1,
+    X11 is X5 - Offset2,
+    X12 is X6 - Offset3,
+
+    write('Computer selected pieces: '), write([(Y4, X10), (Y5, X11), (Y6, X12)]), nl,
     valid_moves(Board, [(X4, Y4), (X5, Y5), (X6, Y6)], ValidMoves),
     length(ValidMoves, Length),
     (Length > 0 -> (X1 = X4, Y1 = Y4, X2 = X5, Y2 = Y5, X3 = X6, Y3 = Y6, random_member(Direction, ValidMoves), write('Computer selected direction: '), write(Direction), nl)
